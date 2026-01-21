01e8d1a5fdccf3878e70a10b62aa97737454aeb8
nicholassud-beep <nicholas.sud@gmail.com>
2026-01-21 02:53:33 -0300

    Update PainelInfoadm+.lua

--- PainelInfoadm+.lua
+++ PainelInfoadm+.lua
@@ -137,7 +137,6 @@ end
 
 -- SISTEMA DE ATUALIZACAO AUTOMATICA (GITHUB)
 -- Configure as URLs abaixo com o seu repositorio
-local update_url = "https://raw.githubusercontent.com/nicholassud-beep/paineladmincvr/main/version.json"
 local script_url = "https://raw.githubusercontent.com/nicholassud-beep/paineladmincvr/main/PainelInfoadm%2B.lua"
 
 function check_update(notify_no_update)
