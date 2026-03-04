3e0e7f2cc120bc78b002f9ee4fdd4d597bbd6377
cvradmin <nicholas.sud@gmail.com>
2026-03-04 14:33:56 -0300

    atualização da staff

--- PainelInfoadm+.lua
+++ PainelInfoadm+.lua
@@ -194,11 +194,11 @@ local weapon_types = {"Todas", "Corpo a corpo", "Pistola", "Espingarda", "Submet
 local staff_by_rank = {
     Desenvolvedor = { "LKWilliam" },
     Direcao = { ".42.", "Boris", "LucasFirmino" },
-    Coordenador = { "Dog.", "Cleyton.", "MatheusUnity", "DiegoN_", "DaviL", "TomRedl." },
+    Coordenador = { "Dog.", "Cleyton.", "MatheusUnity", "DaviL", "TomRedl." },
     Administrador = { "Mirian", "KrugeR.", "Ryu." },
     Moderador = { ".Lacta", "Belo", "Rafael182", "MateusVictor", "chukc", "Dr.Manhattan_.", "Lucasfuj", "Davi.", "LeonardoX", "Caua.." },
     Ajudante = { "Koyama.", "ZeGotinha_.", "NotoriousBIG", "Liima.", "Scheppard", ".RagnaroK", "Faggio", },
-    ["Estagiario"] = { "D0uglitas", "Duarte.Tm", "MitoMitoso", ".PATCHENCO", ".BS", "Freitas_" }
+    ["Estagiario"] = { "D0uglitas", "Duarte.Tm", "MitoMitoso", ".PATCHENCO", ".BS", }
 }
 local staff_nick_to_rank_map = {}
 for rank, nicks in pairs(staff_by_rank) do
@@ -2939,3 +2939,4 @@ function onScriptTerminate(script, quit)
     end
 
 end
+
